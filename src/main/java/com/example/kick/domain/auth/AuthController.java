94ff3ced490481b94c6748ee7d1e44a0b18f49e3
김명진 <4mj.eo@dsm.hs.kr>
2024-10-30 05:10:13 +0900

    refactor: auth 에러 해결

--- src/main/java/com/example/kick/domain/auth/AuthController.java
+++ src/main/java/com/example/kick/domain/auth/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.kick.domain.auth;
 
+import com.example.kick.domain.auth.dto.SignInRequest;
+import com.example.kick.domain.auth.dto.SignUpRequest;
 import com.example.kick.domain.auth.dto.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -20,13 +22,13 @@ public class AuthController {
 
     @Operation(summary = "회원가입")
     @PostMapping
-    public TokenResponse signUp(@RequestBody @Valid String nickname, String password) {
-        return authService.signUp(nickname, password);
+    public TokenResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authService.signUp(request.getNickname(), request.getPassword());
     }
 
     @Operation(summary = "로그인")
     @PostMapping("/signin")
-    public TokenResponse userSignIn(@RequestBody @Valid String nickname, String password) {
-        return authService.signIn(nickname, password);
+    public TokenResponse userSignIn(@RequestBody @Valid SignInRequest request) {
+        return authService.signIn(request.getNickname(), request.getPassword());
     }
 }
