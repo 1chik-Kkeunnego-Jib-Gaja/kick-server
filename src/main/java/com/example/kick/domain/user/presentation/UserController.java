d3e25e0e12ea33bc817a05c55d1fcf2dbbb2ae01
김명진 <4mj.eo@dsm.hs.kr>
2024-10-30 16:38:41 +0900

    feat: 마이페이지 조회

--- src/main/java/com/example/kick/domain/user/presentation/UserController.java
+++ src/main/java/com/example/kick/domain/user/presentation/UserController.java
@@ -1,4 +1,23 @@
 package com.example.kick.domain.user.presentation;
 
+import com.example.kick.domain.user.presentation.dto.UserProfileResponse;
+import com.example.kick.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @Operation(summary = "마이페이지 조회")
+    @GetMapping("/my")
+    public UserProfileResponse getUserProfile() {
+        return userService.getUserProfile();
+    }
 }
